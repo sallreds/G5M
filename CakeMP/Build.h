b307cf89b11627afded1dd9897fca4f610d1ab75
sallreds <44512635+sallreds@users.noreply.github.com>
2021-09-16 17:45:18 +1200

    Updated version and buildtype
    Changed PROJECT_VERSION to v0.1.1-G5S.0 and PROJECT_BUILDTYPE from Development Build to Experimental Build

--- CakeMP/Build.h
+++ CakeMP/Build.h
@@ -2,5 +2,5 @@
 
 #define PROJECT_NAME "CakeMP"
 #define PROJECT_NAME_SHORT "Cake"
-#define PROJECT_VERSION "0.1.000.000"
-#define PROJECT_BUILDTYPE "Development Build"
+#define PROJECT_VERSION "v0.1.1-G5S.0"
+#define PROJECT_BUILDTYPE "Experimental Build"
